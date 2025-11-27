d5a1124d7b7385162964171c94c0dad0044b84e5
4v0n <navodhk@hotmail.com>
2025-11-27 18:20:59 +0000

    fix: block right click on deploy

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 
 if (import.meta.env.VITE_ENVIRONMENT !== "development") {
   document.body.classList.add("disable-selection");
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
